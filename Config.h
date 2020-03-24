f977b4a7d22b1e31962977a6b9b809c0bd221b97
JackLovel <gogkatsu@outlook.com>
2020-03-24 09:21:01 +0800

    add some version information

--- Config.h
+++ Config.h
@@ -3,6 +3,7 @@
 
 #define APP_NAME "白猫更打器"
 #define APP_VERSION "1.0"
+#define BUILD_QT_VERSION "5.14.0"
 
 #define ARTICLE_DIR_FILE "article"
 #define OPEN_PROMPT "F3重打F4载文F5换群CTRL+L乱序回车暂停F8暂停/结束（自由模式）"
